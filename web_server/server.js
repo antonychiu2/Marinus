59f7efb7250d066e603b1e5a31290157ac3ad74d
Antony Chiu <antony@mobb.ai>
2024-07-18 08:35:13 -0600

    Update server.js

--- web_server/server.js
+++ web_server/server.js
@@ -96,7 +96,7 @@ function onError(error) {
         case 'EACCES':
             console.error(bind + ' requires elevated privileges');
             process.exit(1);
-            break;
+            break; 
         case 'EADDRINUSE':
             console.error(bind + ' is already in use');
             process.exit(1);
