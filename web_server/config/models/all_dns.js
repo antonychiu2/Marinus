76a94a79e4c87766bfdead1ac152f3907d354a36
Mobb autofixer <git@mobb.ai>
2025-01-03 17:03:37 +0000

    mobb fix commit: b88db334-388b-4153-896d-24993a793e3f

--- web_server/config/models/all_dns.js
+++ web_server/config/models/all_dns.js
@@ -12,6 +12,7 @@
  * governing permissions and limitations under the License.
  */
 
+const escapeRegExp = require('lodash.escaperegexp');
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
@@ -220,7 +221,7 @@ module.exports = {
         if (subdomain === undefined || subdomain === null || subdomain === 'all') {
             subdomain = '';
         }
-        let reAmazon = new RegExp('^.*' + subdomain + '\.amazonaws\.com');
+        let reAmazon = new RegExp('^.*' + escapeRegExp(subdomain) + '\.amazonaws\.com');
 
         let query = {
             'type': 'cname',
